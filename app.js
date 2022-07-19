ee4509ec2316551f6034d9aa1655bd18322f7029
Sujit Roy <89136332+iamsujitroy@users.noreply.github.com>
2022-07-19 22:13:27 +0530

    replaced full-stack with front-end, some changes on about me and changed the skill icons

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // typing animation starts here
 const professionText = document.querySelector(".profession-container h2");
-let textContent = ["I am a web developer ", "I like to code "];
+let textContent = ["I am a Fron-End Developer ", "I like to code "];
 let i = 0;
 let j = 0;
 let currentTextContent = [];
